aef671215ceb028fae8c2ed2889ec8ef807c5169
Mari Calmer <superlassig@gmail.com>
2023-02-28 21:12:20 +0100

    dist folder for main.js but no gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: "./src/index.js",
   mode: "development",
   output: {
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, "dist"),
     filename: "main.js"
   },
   devtool: "sourcemap",
