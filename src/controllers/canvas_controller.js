1d68a85d0744f2b6333bc60b41dd7d5bc3bc266f
Mari Calmer <superlassig@gmail.com>
2023-03-01 18:14:28 +0100

    change code for canvas so that it resizes and integrates touch events + minimum design

--- src/controllers/canvas_controller.js
+++ src/controllers/canvas_controller.js
@@ -3,18 +3,36 @@ import { gsap } from "gsap";
 
 const signs = [];
 const mouse = { x: 0, y: 0 };
-const gridLength = 9;
+const gridLength = 15;
 let mouseMoved = false;
 let mouseOver = false;
 const c = document.getElementById("c");
 const context = c.getContext("2d");
+c.width = window.innerWidth;
+c.height = window.innerHeight;
+let windowResizeDebouncing = null;
+let mouseMoveDebouncing = null;
+
+function registerCursorMove(event) {
+  if (!mouseMoveDebouncing) {
+    setTimeout(() => {
+      if (event.targetTouches && event.targetTouches[0]) {
+        event = event.targetTouches[0];
+      }
+      const rect = c.getBoundingClientRect();
+      mouse.x = event.clientX - rect.left;
+      mouse.y = event.clientY - rect.top;
+      mouseMoved = true;
+      mouseMoveDebouncing = null;
+    }, 10);
+  }
+  mouseMoveDebouncing = event;
+}
 
 export default class extends Controller {
   connect() {
-    console.log("canvas controller connected");
-
     class Plus {
-      constructor(x, y, left, top, width, height, scale) {
+      constructor() {
         this.x = 0;
         this.y = 0;
 
@@ -28,45 +46,59 @@ export default class extends Controller {
       }
     }
 
-    Plus.prototype.draw = function () {
-      context.setTransform(this.scale, 0, 0, this.scale, this.left + this.x, this.top + this.y);
+    Plus.prototype.draw = function drawing() {
+      context.save();
+      context.beginPath();
+      context.setTransform(
+        this.scale,
+        0,
+        0,
+        this.scale,
+        this.left + this.x,
+        this.top + this.y
+      );
+      context.lineWidth = 2;
 
       context.moveTo(0, -this.height / 2);
       context.lineTo(0, this.height / 2);
 
       context.moveTo(-this.width / 2, 0);
       context.lineTo(this.width / 2, 0);
+
+      context.stroke();
+      context.closePath();
+      context.restore();
     };
 
     for (let i = 0; i < gridLength; i += 1) {
       signs[i] = [];
-
       for (let j = 0; j < gridLength; j += 1) {
-        const sign = new Plus();
-
-        sign.left = c.width / (gridLength + 1) * (i + 1);
-        sign.top = c.height / (gridLength + 1) * (j + 1);
-
-        sign.width = 10;
-        sign.height = 10;
-
-        signs[i][j] = sign;
+        const min = Math.min(c.width, c.height);
+        signs[i][j] = new Plus();
+        signs[i][j].left = c.width / (gridLength + 1) * (i + 1);
+        signs[i][j].top = c.height / (gridLength + 1) * (j + 1);
+        signs[i][j].width = min / 100;
+        signs[i][j].height = min / 100;
       }
+    }
+
+    function dist([x1, y1], [x2, y2]) {
+      const dx = x1 - x2;
+      const dy = y1 - y2;
+      return Math.sqrt(dx * dx + dy * dy) || 1;
     }
 
     function calculateIconPosition() {
       for (let i = 0; i < gridLength; i += 1) {
         for (let j = 0; j < gridLength; j += 1) {
           const sign = signs[i][j];
-          let radius = 20;
-          const dx = mouse.x - sign.left;
-          const dy = mouse.y - sign.top;
-          const dist = Math.sqrt(dx * dx + dy * dy) || 1;
-
-          const angle = Math.atan2(dy, dx);
-
-          if (dist < radius) {
-            radius = dist;
+          let hyp = Math.min(c.width, c.height) / (gridLength + 1) / 2;
+          const d = dist([sign.left, sign.top], [mouse.x, mouse.y]);
+          const ax = mouse.x - sign.left;
+          const ay = mouse.y - sign.top;
+          const angle = Math.atan2(ay, ax);
+          if (d < hyp + sign.width) {
+            hyp = d;
             gsap.to(sign, { duration: 0.3, scale: 2 });
           } else {
             gsap.to(sign, { duration: 0.3, scale: 1 });
@@ -74,32 +106,28 @@ export default class extends Controller {
 
           gsap.to(sign, {
             duration: 0.3,
-            x: Math.cos(angle) * radius,
-            y: Math.sin(angle) * radius
+            x: Math.cos(angle) * hyp,
+            y: Math.sin(angle) * hyp
           });
         }
       }
     }
 
     function draw() {
+      context.clearRect(0, 0, c.width, c.height);
+      context.strokeStyle = "white";
+
       if (mouseOver && mouseMoved) {
         calculateIconPosition();
         mouseMoved = false;
       }
-      calculateIconPosition();
-      context.clearRect(0, 0, c.width, c.height);
-      context.save();
-      context.beginPath();
+
       for (let i = 0; i < gridLength; i += 1) {
         for (let j = 0; j < gridLength; j += 1) {
           const sign = signs[i][j];
-
           sign.draw(context);
         }
       }
-      context.closePath();
-      context.restore();
-      context.stroke();
     }
     gsap.ticker.add(draw);
   }
@@ -109,11 +137,12 @@ export default class extends Controller {
   }
 
   mouseMove(event) {
-    const rect = c.getBoundingClientRect();
-    mouse.x = event.clientX - rect.left;
-    mouse.y = event.clientY - rect.top;
+    registerCursorMove(event);
+  }
 
-    mouseMoved = true;
+  touchStart(event) {
+    mouseOver = true;
+    registerCursorMove(event);
   }
 
   mouseOverFalse() {
@@ -121,13 +150,36 @@ export default class extends Controller {
     for (let i = 0; i < gridLength; i += 1) {
       for (let j = 0; j < gridLength; j += 1) {
         const sign = signs[i][j];
-        gsap.to(sign, {
-          duration: 0.3,
-          x: 0,
-          y: 0,
-          scale: 1
-        });
+        if (!mouseOver) {
+          gsap.to(sign, {
+            duration: 0.3,
+            x: 0,
+            y: 0,
+            scale: 1
+          });
+        }
       }
     }
+  }
+
+  resizeCanvas(event) {
+    if (!windowResizeDebouncing) {
+      setTimeout(() => {
+        c.width = window.innerWidth;
+        c.height = window.innerHeight;
+        for (let i = 0; i < gridLength; i += 1) {
+          for (let j = 0; j < gridLength; j += 1) {
+            const min = Math.min(c.width, c.height);
+            const sign = signs[i][j];
+            sign.left = c.width / (gridLength + 1) * (i + 1);
+            sign.top = c.height / (gridLength + 1) * (j + 1);
+            sign.width = min / 100;
+            sign.height = min / 100;
+          }
+        }
+        windowResizeDebouncing = null;
+      }, 250);
+    }
+    windowResizeDebouncing = event;
   }
 }
